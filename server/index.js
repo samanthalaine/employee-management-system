cfd64a26ecd18a20a6ab12b12449065f39823a93
Samantha Laine <samanthalaine8@gmail.com>
2021-11-23 00:55:26 -0500

    made request to get an employee

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ const pool = require("./db");
 const dotenv = require("dotenv");
 dotenv.config();
 
-
 //middleware
 app.use(cors());
 app.use(express.json()); //req.body
@@ -14,11 +13,11 @@ app.use(express.json()); //req.body
 
 app.post("/create", async (req, res) => {
   try {
-    const { name, age, country, position, wage} = req.body;
+    const { name, age, country, position, wage } = req.body;
 
     const newEmployee = await pool.query(
       "INSERT INTO employees (name, age, country, position, wage) VALUES($1,$2,$3,$4,$5) RETURNING *",
-    [name, age, country, position, wage],
+      [name, age, country, position, wage]
     );
     res.json(newEmployee.rows);
   } catch (err) {
@@ -27,6 +26,7 @@ app.post("/create", async (req, res) => {
 });
 
 //get all employees
+
 app.get("/employees", async (req, res) => {
   try {
     const allEmployees = await pool.query("SELECT * FROM employees");
@@ -38,16 +38,26 @@ app.get("/employees", async (req, res) => {
 
 // get an employee
 
+app.get("/employees/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const employee = await pool.query(
+      "SELECT * FROM employees WHERE employee_id = $1",
+      [id]
+    );
+
+    res.json(employee.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
 // update an employee
 
 // delete an employee
 
+const PORT = process.env.PORT || 3001;
 
-
-
-const PORT = process.env.PORT || 3001
-
-app.listen(PORT, ()=>{
-    console.log(`Server is running on ${PORT}`)
-})
-
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
