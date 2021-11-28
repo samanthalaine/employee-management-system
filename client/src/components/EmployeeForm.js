c5c9d0c6c890a0cafb1be3c581b9e498e7abded3
Samantha Laine <samanthalaine8@gmail.com>
2021-11-27 20:38:46 -0500

    improved add employee function

--- client/src/components/EmployeeForm.js
+++ client/src/components/EmployeeForm.js
@@ -1,23 +1,23 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function EmployeeForm() {
-    const [name, setName] = useState("")
-    const [age, setAge] = useState(0);
-    const [country, setCountry] = useState("");
-    const [position, setPosition] = useState("");
-    const [wage, setWage] = useState(0);
-    
-    const addEmployee = () =>{
-      const employee = { name, age, country, position, wage }
-      axios.post('http://localhost:3001/create', employee).then(()=>{
-        console.log('success')
-      })
-    }
-     
-    return (
-        <form class="w-full max-w-sm mt-5">
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState("");
+  const [position, setPosition] = useState("");
+  const [wage, setWage] = useState(0);
+
+  const addEmployee = async (e) => {
+    const employee = { name, age, country, position, wage };
+    e.preventDefault();
+    await axios.post("http://localhost:3001/create", employee).then(() => {
+      console.log("success");
+    });
+  };
+
+  return (
+    <form onSubmit={addEmployee} class="w-full max-w-sm mt-5">
       <div class="md:flex md:items-center mb-6">
         <div class="md:w-1/3">
           <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
@@ -29,7 +29,7 @@ function EmployeeForm() {
             class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
             type="text"
             placeholder="Name"
-            onChange={(e) =>setName(e.target.value)}
+            onChange={(e) => setName(e.target.value)}
           />
         </div>
       </div>
@@ -44,7 +44,7 @@ function EmployeeForm() {
             class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
             type="number"
             placeholder="Age"
-            onChange={(e) =>setAge(e.target.value)}
+            onChange={(e) => setAge(e.target.value)}
           />
         </div>
       </div>
@@ -59,7 +59,7 @@ function EmployeeForm() {
             class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
             type="text"
             placeholder="Country"
-            onChange={(e) =>setCountry(e.target.value)}
+            onChange={(e) => setCountry(e.target.value)}
           />
         </div>
       </div>
@@ -74,7 +74,7 @@ function EmployeeForm() {
             class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
             type="text"
             placeholder="Position"
-            onChange={(e) =>setPosition(e.target.value)}
+            onChange={(e) => setPosition(e.target.value)}
           />
         </div>
       </div>
@@ -89,20 +89,20 @@ function EmployeeForm() {
             class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
             type="number"
             placeholder="Wage"
-            onChange={(e) =>setWage(e.target.value)}
+            onChange={(e) => setWage(e.target.value)}
           />
         </div>
       </div>
       <div class="md:flex md:items-center">
         <div class="md:w-1/3"></div>
         <div class="md:w-2/3">
-          <button onClick={addEmployee} class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">
+          <button class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">
             Add Employee
           </button>
         </div>
       </div>
     </form>
-    )
+  );
 }
 
-export default EmployeeForm
+export default EmployeeForm;
