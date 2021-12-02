f26696866555e7e24215fe365d7985510c6d072c
Samantha Laine <samanthalaine8@gmail.com>
2021-12-01 19:09:09 -0500

    implemented authentication using auth0

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,21 @@ import EmployeeTable from "./components/EmployeeTable";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import { Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import Auth0ProviderWithHistory from "./auth0Provider";
 
 function App() {
   return (
-    <div className="">
-    <Navbar />
-
-    <Routes>
-      <Route path="/" exact element={<Home />} />
-      <Route path="employeeform/*" element={<EmployeeForm />} />
-      <Route path="employeetable/*" element={<EmployeeTable />} />
-      
-    </Routes>
-  </div>
+    <BrowserRouter>
+      <Auth0ProviderWithHistory>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="employeeform/*" element={<EmployeeForm />} />
+          <Route path="employeetable/*" element={<EmployeeTable />} />
+        </Routes>
+      </Auth0ProviderWithHistory>
+    </BrowserRouter>
   );
 }
 
