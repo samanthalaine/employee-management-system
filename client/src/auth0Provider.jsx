fa97c9d07cad2587d4c1ff3a34b6bbc80e8c2792
Samantha <samanthalaine8@gmail.com>
2025-11-11 17:06:25 -0500

    test

--- client/src/auth0Provider.jsx
+++ client/src/auth0Provider.jsx
@@ -6,7 +6,6 @@ const Auth0ProviderWithHistory = ({ children }) => {
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
 
-  
   return (
     <Auth0Provider
       domain={domain}
